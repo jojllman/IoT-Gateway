8c5d45b5e30101a3effe7df065fe13dbf40ba9db
Jing-Lin Wang <jojllman@gmail.com>
2016-03-29 10:52:38 +0800

    新增JSON paser

--- src/tw/edu/ntust/connectivitylab/jojllman/kura/iotgateway/device/DeviceManager.java
+++ src/tw/edu/ntust/connectivitylab/jojllman/kura/iotgateway/device/DeviceManager.java
@@ -3,8 +3,18 @@ package tw.edu.ntust.connectivitylab.jojllman.kura.iotgateway.device;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author jojllman
+ * This is a class for managing device and maintain device profile. Also give device list 
+ * to users.
+ *
+ */
 
 public class DeviceManager {
 	private static final Logger s_logger = LoggerFactory.getLogger(DeviceManager.class);
@@ -21,7 +31,21 @@ public class DeviceManager {
 		}
 	}
 	
-	public void insertDeviceProfile(IDeviceProfile profile) {
+	private void parseDeviceAdvertisementMessage(byte[] data) {
+		String parseData = new String(data);
+		
+		JSONObject jsonObject = new JSONObject(parseData);
+		
+		String name = jsonObject.getString("Name");
+		String description = jsonObject.getString("Description");
+		JSONObject deviceMain = jsonObject.getJSONObject("Detail");
+		String type = deviceMain.getString("Type");
+		String uuid = deviceMain.getString("UUID");
+		
+		
+	}
+	
+	private void insertDeviceProfile(IDeviceProfile profile) {
 		m_deviceProfiles.add(profile);
 	}
 	
